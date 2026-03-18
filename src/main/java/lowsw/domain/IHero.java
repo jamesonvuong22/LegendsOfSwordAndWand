e9907c413593b0a18df7831fc124f66e83c29ecc
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:41:09 -0400

    Add IHero interface with basic hero attributes

--- src/main/java/lowsw/domain/IHero.java
+++ src/main/java/lowsw/domain/IHero.java
@@ -1,1 +1,14 @@
+package lowsw.domain;
+public interface IHero {
+    String getName();
+    ClassType getClassType();
+    int getLevel();
+    int getHp();
+    int getMana();
+    int getAttackPower();
+    int getDefensePower();
+    void setHp(int value);
+    void setMana(int value);
+    default boolean isAlive() { return getHp() > 0; }
+}
 
