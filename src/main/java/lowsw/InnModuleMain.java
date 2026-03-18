08c9787707fc3d9a52d4cd41f844598ff8f95ff3
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:33:08 -0400

    Implement main method for InnModule

--- src/main/java/lowsw/InnModuleMain.java
+++ src/main/java/lowsw/InnModuleMain.java
@@ -1,1 +1,23 @@
+package lowsw;
+
+import lowsw.domain.ClassType;
+import lowsw.domain.User;
+import lowsw.persistence.InMemoryCampaignRepository;
+import lowsw.service.CampaignService;
+import lowsw.service.DefaultHeroFactory;
+import lowsw.service.InnService;
+
+// With the use of AI
+public class InnModuleMain {
+    public static void main(String[] args) {
+        DefaultHeroFactory factory = new DefaultHeroFactory();
+        CampaignService campaignService = new CampaignService(new InMemoryCampaignRepository(), factory);
+        var campaign = campaignService.startNew(new User(1, "dave", "HASH::pw"), ClassType.MAGE);
+        InnService innService = new InnService(factory);
+        innService.rest(campaign);
+        innService.buyBread(campaign);
+        innService.recruit(campaign, ClassType.ORDER, "Knight");
+        System.out.println("Party size after inn: " + campaign.getParty().getHeroes().size());
+    }
+}
 
