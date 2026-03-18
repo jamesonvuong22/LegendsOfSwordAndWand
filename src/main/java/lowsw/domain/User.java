10b82451a65b23f2e93da5e8407aed35bc29401b
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:43:14 -0400

    Add User class with fields and constructor

--- src/main/java/lowsw/domain/User.java
+++ src/main/java/lowsw/domain/User.java
@@ -1,1 +1,15 @@
+package lowsw.domain;
+public class User {
+    private final long id;
+    private final String username;
+    private final String passwordHash;
+    public User(long id, String username, String passwordHash) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+    public long getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPasswordHash() { return passwordHash; }
+}
 
