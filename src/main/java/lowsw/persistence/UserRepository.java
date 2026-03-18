d7b5b441ec6badebf1a16b7d504040bd075c403a
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:47:37 -0400

    Add UserRepository interface for user operations

--- src/main/java/lowsw/persistence/UserRepository.java
+++ src/main/java/lowsw/persistence/UserRepository.java
@@ -1,1 +1,9 @@
+package lowsw.persistence;
+
+import lowsw.domain.User;
+
+public interface UserRepository {
+    User create(String username, String passwordHash);
+    User findByUsername(String username);
+}
 
