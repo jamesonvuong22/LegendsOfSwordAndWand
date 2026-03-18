8845928c2c68a9d32393d7eaca258a3e6870cb44
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:37:50 -0400

    Implement InnController with service methods

--- src/main/java/lowsw/controller/InnController.java
+++ src/main/java/lowsw/controller/InnController.java
@@ -1,1 +1,14 @@
+package lowsw.controller;
+
+import lowsw.domain.CampaignState;
+import lowsw.domain.ClassType;
+import lowsw.service.InnService;
+
+public class InnController {
+    private final InnService innService;
+    public InnController(InnService innService) { this.innService = innService; }
+    public void rest(CampaignState state) { innService.rest(state); }
+    public void buyBread(CampaignState state) { innService.buyBread(state); }
+    public void recruit(CampaignState state, ClassType type, String name) { innService.recruit(state, type, name); }
+}
 
