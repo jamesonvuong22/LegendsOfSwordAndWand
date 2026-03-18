b51fc8796f7116e7f9a0ff389f4aa5a8ff9e5409
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:34:38 -0400

    Implement PvP module main functionality

--- src/main/java/lowsw/PvPModuleMain.java
+++ src/main/java/lowsw/PvPModuleMain.java
@@ -1,1 +1,23 @@
+package lowsw;
+
+import lowsw.domain.ClassType;
+import lowsw.domain.Party;
+import lowsw.persistence.InMemoryPvPRepository;
+import lowsw.service.DefaultHeroFactory;
+import lowsw.service.PvPService;
+
+public class PvPModuleMain {
+    public static void main(String[] args) {
+        PvPService pvpService = new PvPService(new InMemoryPvPRepository());
+        var invite = pvpService.sendInvitation("jameson", "dave");
+        pvpService.acceptInvitation(invite.getId());
+
+        DefaultHeroFactory factory = new DefaultHeroFactory();
+        Party a = new Party(0); a.addHero(factory.createHero(ClassType.ORDER, "A", 1));
+        Party b = new Party(0); b.addHero(factory.createHero(ClassType.CHAOS, "B", 1));
+        var state = pvpService.startBattle(a, b);
+        System.out.println("PvP invite status: " + invite.getStatus());
+        System.out.println("PvP battle phase: " + state.getPhase().name());
+    }
+}
 
