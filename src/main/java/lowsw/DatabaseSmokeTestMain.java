7c85bca499db2e21e2c3dd58d987490d62f07a55
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:21:03 -0400

    Refactor DatabaseSmokeTestMain to use Db singleton
    Refactored main method to use singleton pattern for Db instance.

--- src/main/java/lowsw/DatabaseSmokeTestMain.java
+++ src/main/java/lowsw/DatabaseSmokeTestMain.java
@@ -3,14 +3,11 @@ package lowsw;
 import lowsw.persistence.Db;
 import lowsw.persistence.DbConfig;
 
-import java.sql.Connection;
-
-// With the use of AI
 public class DatabaseSmokeTestMain {
-    public static void main(String[] args) throws Exception {
-        Db db = new Db(new DbConfig());
-        try (Connection c = db.getConnection()) {
-            System.out.println("DB connection OK: " + c.getMetaData().getURL());
-        }
+    public static void main(String[] args) {
+        Db db = Db.getInstance();
+        DbConfig config = new DbConfig();
+        System.out.println("DB singleton ready for URL: " + config.url());
+        System.out.println("DB instance: " + db.getClass().getSimpleName());
     }
 }
