cbf24d36047ae18958a09945e17612a79b0413e7
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:52:17 -0400

    Implement PvPService for handling invitations and battles

---
+++ src/main/java/lowsw/service/PvPService.java
@@ -0,0 +1,22 @@
+package lowsw.service;
+
+import lowsw.domain.Party;
+import lowsw.domain.PvPInvitation;
+import lowsw.persistence.PvPRepository;
+
+public class PvPService {
+    private final PvPRepository pvpRepository;
+    public PvPService(PvPRepository pvpRepository) { this.pvpRepository = pvpRepository; }
+
+    public PvPInvitation sendInvitation(String challenger, String opponent) {
+        return pvpRepository.createInvite(challenger, opponent);
+    }
+
+    public PvPInvitation acceptInvitation(long inviteId) {
+        return pvpRepository.accept(inviteId);
+    }
+
+    public BattleState startBattle(Party challengerParty, Party opponentParty) {
+        return new BattleEngine(new BasicDamageStrategy()).initBattle(challengerParty, opponentParty);
+    }
+}
