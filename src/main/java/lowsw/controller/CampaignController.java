ccb5f8e58bb09b22f369e32002dc53aaf5eb3dcb
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:37:16 -0400

    Add CampaignController with campaign management methods

--- src/main/java/lowsw/controller/CampaignController.java
+++ src/main/java/lowsw/controller/CampaignController.java
@@ -1,1 +1,15 @@
+package lowsw.controller;
+
+import lowsw.domain.CampaignState;
+import lowsw.domain.ClassType;
+import lowsw.domain.User;
+import lowsw.service.CampaignService;
+
+public class CampaignController {
+    private final CampaignService campaignService;
+    public CampaignController(CampaignService campaignService) { this.campaignService = campaignService; }
+    public CampaignState startNew(User user, ClassType classType) { return campaignService.startNew(user, classType); }
+    public CampaignState continueCampaign(long userId) { return campaignService.continueCampaign(userId); }
+    public void exit(CampaignState state) { campaignService.exit(state); }
+}
 
