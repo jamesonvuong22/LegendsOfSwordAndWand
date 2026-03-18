7e97b67e60e327f6f3c3dac12e15cf330beaf864
Jameson Vuong <jamv@my.yorku.ca>
2026-03-18 15:40:25 -0400

    Add CampaignState class with user and game state

--- src/main/java/lowsw/domain/CampaignState.java
+++ src/main/java/lowsw/domain/CampaignState.java
@@ -1,1 +1,29 @@
+package lowsw.domain;
+public class CampaignState {
+    private final long userId;
+    private final String username;
+    private final Party party;
+    private int roomNumber;
+    private String lastRoomType;
+    private boolean inProgress;
+
+    public CampaignState(long userId, String username, Party party, int roomNumber, String lastRoomType, boolean inProgress) {
+        this.userId = userId;
+        this.username = username;
+        this.party = party;
+        this.roomNumber = roomNumber;
+        this.lastRoomType = lastRoomType;
+        this.inProgress = inProgress;
+    }
+
+    public long getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public Party getParty() { return party; }
+    public int getRoomNumber() { return roomNumber; }
+    public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
+    public String getLastRoomType() { return lastRoomType; }
+    public void setLastRoomType(String lastRoomType) { this.lastRoomType = lastRoomType; }
+    public boolean isInProgress() { return inProgress; }
+    public void setInProgress(boolean inProgress) { this.inProgress = inProgress; }
+}
 
