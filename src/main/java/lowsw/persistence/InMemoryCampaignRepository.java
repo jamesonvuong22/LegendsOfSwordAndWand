8705e3fcce417b0e5425c6081a3f7d451233234d
Jameson Vuong <jamv@my.yorku.ca>
2026-03-29 17:20:36 -0400

    Refactor InMemoryCampaignRepository to implement ICampaignRepository

--- src/main/java/lowsw/persistence/InMemoryCampaignRepository.java
+++ src/main/java/lowsw/persistence/InMemoryCampaignRepository.java
@@ -5,8 +5,16 @@ import java.util.Map;
 
 import lowsw.domain.CampaignState;
 
-public class InMemoryCampaignRepository implements CampaignRepository {
+public class InMemoryCampaignRepository implements ICampaignRepository {
     private final Map<Long, CampaignState> campaigns = new HashMap<>();
-    @Override public void save(CampaignState state) { campaigns.put(state.getUserId(), state); }
-    @Override public CampaignState loadLatest(long userId) { return campaigns.get(userId); }
+
+    @Override
+    public void save(CampaignState state) {
+        campaigns.put(state.getUserId(), state);
+    }
+
+    @Override
+    public CampaignState loadLatest(long userId) {
+        return campaigns.get(userId);
+    }
 }
