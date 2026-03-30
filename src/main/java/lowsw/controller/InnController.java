75fe1699b84873f52c3df8026af90691971c2eb9
Jameson Vuong <jamv@my.yorku.ca>
2026-03-30 17:31:00 -0400

    Refactor InnController to add item handling methods

--- src/main/java/lowsw/controller/InnController.java
+++ src/main/java/lowsw/controller/InnController.java
@@ -6,9 +6,24 @@ import lowsw.service.InnService;
 
 public class InnController {
     private final InnService innService;
-    public InnController(InnService innService) { this.innService = innService; }
-    public void rest(CampaignState state) { innService.rest(state); }
-    public void buyBread(CampaignState state) { innService.buyBread(state); }
-    public void recruit(CampaignState state, ClassType type, String name) { innService.recruit(state, type, name); }
-}
 
+    public InnController(InnService innService) {
+        this.innService = innService;
+    }
+
+    public void rest(CampaignState state) {
+        innService.rest(state);
+    }
+
+    public boolean buyItem(CampaignState state, String itemName, int cost) {
+        return innService.buyItem(state, itemName, cost);
+    }
+
+    public boolean useItem(CampaignState state, String itemName, int heroIndex) {
+        return innService.useItem(state, itemName, heroIndex);
+    }
+
+    public boolean recruitHero(CampaignState state, ClassType type, String name, int cost) {
+        return innService.recruitHero(state, type, name, cost);
+    }
+}
